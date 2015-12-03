df9495cfb61823c5054ca6c00e2c213c68ad2e32
James Halliday <mail@substack.net>
2015-12-03 00:52:14 -0600

    using hyperkv, replication works

--- main.js
+++ main.js
@@ -5,47 +5,34 @@ var wsock = require('websocket-stream')
 var through = require('through2')
 
 var level = require('level-browserify')
-var db = level('whatever')
-var forkdb = require('forkdb')
-var fdb = forkdb(db, {
-  store: require('idb-content-addressable-blob-store')()
-})
+var hyperlog = require('hyperlog')
+var hyperkv = require('hyperkv')
 
-fdb.on('create', function (hash) {
-  fdb.get(hash, function (err, meta) {
-    if (err) return console.error(err)
-    fdb.createReadStream(hash).pipe(concat(readNode(meta.key)))
+var db = level('map')
+var log = hyperlog(db, { valueEncoding: 'json' })
+
+var kv = hyperkv({ db: db, log: log })
+
+kv.on('update', function (key, value, node) {
+  kv.get(key, function (err, values) {
+    if (err) console.error(err)
+    loop.state.nodes[key] = values
+    loop.update(loop.state)
   })
 })
 
 var vdom = require('virtual-dom'), h = vdom.h
 var main = require('main-loop')
-var loop = main({ nodes: [], ways: [] }, render, vdom)
+var loop = main({ nodes: {}, ways: {} }, render, vdom)
 
 var root = document.querySelector('#content')
 root.replaceChild(loop.target, root.childNodes[0])
 
-fdb.keys(function (err, keys) {
-  if (err) return console.error(err)
-  keys.forEach(readKey)
-})
-
-function readKey (key) {
-  fdb.forks(key.key, function (err, heads) {
-    heads.forEach(function (head) {
-      fdb.createReadStream(head.hash).pipe(concat(readNode(key.key)))
-    })
+kv.createReadStream()
+  .on('data', function (row) {
+    loop.state.nodes[row.key] = row.values
+    loop.update(loop.state)
   })
-}
-
-function readNode (key) {
-  return function (body) {
-    var node = JSON.parse(body)
-    loop.update(xtend(loop.state, {
-      nodes: loop.state.nodes.concat({ key: key, value: node })
-    }))
-  }
-}
 
 function render (state) {
   return h('div', [
@@ -74,14 +61,20 @@ function render (state) {
     ]),
     h('div', [
       h('h2', 'nodes'),
-      h('div', state.nodes.map(function (node) {
-        return h('div', [
-          'id=' + node.key,
-          ' ',
-          'lat=' + node.value.lat,
-          ' ',
-          'lon=' + node.value.lon
-        ])
+      h('div', Object.keys(state.nodes).map(function (key) {
+        var values = state.nodes[key]
+        return h('div',
+          Object.keys(values).map(function (link) {
+            var node = values[link]
+            return h('div', [
+              'id=' + key,
+              ' ',
+              'lat=' + node.lat,
+              ' ',
+              'lon=' + node.lon
+            ])
+          })
+        )
       })),
       h('h2', 'ways')
       // ...
@@ -91,21 +84,17 @@ function render (state) {
   function create (ev) {
     ev.preventDefault()
     var id = randombytes(8).toString('hex')
-    var node = {
+    var value = {
       lat: this.elements.lat.value,
       lon: this.elements.lon.value,
     }
-    fdb.forks(id, function (err, heads) {
-      var meta = { key: id, prev: heads }
-      var w = fdb.createWriteStream(meta, function (err, key) {
-        if (err) return console.error(err)
-      })
-      w.end(JSON.stringify(node))
+    kv.put(id, value, function (err, node) {
+      if (err) console.error(err)
     })
   }
   function replicate (ev) {
     ev.preventDefault()
     var stream = wsock(this.elements.url.value)
-    stream.pipe(fdb.replicate()).pipe(stream)
+    stream.pipe(log.replicate()).pipe(stream)
   }
 }
